b7fcbdcef6a72a904407597d67ecc37bb4cee26f
Xincheng Huang <xh721@nyu.edu>
2018-02-04 20:27:31 -0500

    Documenting

---
+++ Week2/week2_assignment/README.md
@@ -0,0 +1,19 @@
+# Week2 Assignment - Bluetooth controlled tic-tac-toe
+
+## Introduction
+For this week's assignment, I come up with an idea of makeing a tic-tac-toe game on a breadboard that can be controled by a smart phone through bluetooth. The board for this tic-tac-toe board is 3 * 3 in dimension, and is built with 9 pairs of LEDs (pictures are shown below). In this project, there is one service, called "TICTACTOE" with, two characteristics, "Move" and "Start". To play the game, a player would first click the "Start" characteristic, and input a random value to start the game. After that, the player can put down his move by inputing a position(A0, B2, etc), then the computer(microprocessor) would respond, and so on and so forth. A player can restart the game by operate in the "Start" characteristic once again, either in the middle of a game, or when the game naturally ends.
+
+## Service, characteristics and UUIDs
+As mentioned above, the service, characteristics and UUIDs are as the following:
+- Service: TICTACTOE; UUID: FF20
+- Characteristic 1: MOVE; UUID: FF21
+	- Type: Read | Write
+	- Valid inputs: A0, A1, A2, B0, B1, B2, C0, C1, C2
+	- Description: Using this, a user controls his/her moves by inputing a valid position mark
+- Characteristic 2: (Re)Start; UUID: FF22
+	- Type: READ | Write
+	- Valid inputs: any
+	- Description: A user can start or restart the game using this characteristic by inputting any character.
+![Service](documentation/service.PNG)
+
+## 
---
+++ Week2/week2_assignment/week2_assignment.ino
@@ -0,0 +1,237 @@
+// tic-tac-toe (ttt)
+#include <CurieBLE.h>
+
+// an array representing the 3*3 light matrix, where 0 means off,
+// 1 means green, and 2 means yellow
+int matrix[9];
+// corresponding values
+int values[9] = {160, 161, 162, 176, 177, 178, 192, 193, 194};
+
+// status
+int state = 0; // 0 being standing-by, 1 being in-game
+
+// setting up peripheral, services and descriptors
+BLEPeripheral blePeripheral;
+BLEService gameService = BLEService("FF20");
+BLEUnsignedCharCharacteristic moveCharacteristic = BLEUnsignedCharCharacteristic("FF21", BLERead | BLEWrite);
+BLEUnsignedCharCharacteristic startCharacteristic = BLEUnsignedCharCharacteristic("FF22", BLERead | BLEWrite);
+BLEDescriptor moveDescriptor = BLEDescriptor("2901", "Move");
+BLEDescriptor startDescriptor = BLEDescriptor("2901", "(Re)start");
+
+void setup() {
+  Serial.begin(9600);
+
+  // setting up pins
+  for (int i = 2; i < 14; i++) {
+    pinMode(i, OUTPUT);
+  }
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+
+  // initialize matrix
+  for (int i = 0; i < 9; i++) {
+    matrix[i] = 0;
+  }
+
+  // setting up peripheral: names, attributes and event handlers
+  blePeripheral.setLocalName("TICTACTOE");
+  blePeripheral.setDeviceName("TICTACTOE");
+  blePeripheral.addAttribute(gameService);
+  blePeripheral.addAttribute(moveCharacteristic);
+  blePeripheral.addAttribute(moveDescriptor);
+  blePeripheral.addAttribute(startCharacteristic);
+  blePeripheral.addAttribute(startDescriptor);
+
+  moveCharacteristic.setEventHandler(BLEWritten, moveCharacteristicWritten);
+  startCharacteristic.setEventHandler(BLEWritten, startCharacteristicWritten);
+
+  blePeripheral.setAdvertisedServiceUuid(gameService.uuid());
+  blePeripheral.begin();
+}
+
+void loop() {
+  blePeripheral.poll();
+}
+
+void startCharacteristicWritten(BLECentral&central, BLECharacteristic&characteristic) {
+  // plays and resets the LEDs, and also revert the state
+  gameDisplay();
+  if (state == 0){
+    state = 1;  
+  }
+}
+
+void moveCharacteristicWritten(BLECentral&central, BLECharacteristic&characteristic) {
+  // only active when the state is in-game(1)
+  if (state == 1) {
+    int pos;
+    if (pos = moveCharacteristic.value()) {
+      for (int i = 0; i < 9; i++) {
+        if (pos == values[i] && matrix[i] == 0) {
+          // the input is valid, move
+          indexToPinMapper(i, 0);
+          matrix[i] = 1;
+          Serial.print(F("Player moved: "));
+          Serial.println(moveCharacteristic.value(), HEX);
+          delay(500);
+          computerMove();
+          break;
+        }
+        if (i == 8) {
+          // the input is invalid, notify?
+          Serial.println(F("Invalid input"))  ;
+        }
+      }
+    }
+  }
+}
+
+// make a move automatically when it is computer's turn
+void computerMove() {
+  resultInspector();
+  // only active when the state is in-game(1)
+  if (state == 1) {
+    // just stupidly picking the first valid move
+    while (true) {
+      int pos = random(0, 9);
+      if (matrix[pos] == 0) {
+        matrix[pos] = 2;
+        indexToPinMapper(pos, 1);
+        Serial.print(F("Computer moved: "));
+        Serial.println(values[pos], HEX);
+        resultInspector();
+        break;
+      }
+    }
+  }
+}
+
+// maps the matrix index (0 ... 8) to the corresponding led pin, and output HIGH directly
+void indexToPinMapper(int index, int side) {
+  index = (index + 1) * 2;
+  if (side == 1) {
+    // increment by offset 1 if it is computer's turn
+    index++;
+  }
+  if (index <= 13) {
+    digitalWrite(index, HIGH);
+  } else {
+    switch (index) {
+      case 14:
+        analogWrite(A5, 255);
+        break;
+      case 15:
+        analogWrite(A4, 255);
+        break;
+      case 16:
+        analogWrite(A3, 255);
+        break;
+      case 17:
+        analogWrite(A2, 255);
+        break;
+      case 18:
+        analogWrite(A1, 255);
+        break;
+      case 19:
+        analogWrite(A0, 255);
+        break;
+      default:
+        Serial.println("What?!");
+    }
+  }
+}
+
+void resultInspector() {
+  // only active when the state is in-game(1)
+  if (state == 1) {
+    // inspect lines
+    for (int i = 0; i < 7; i += 3) {
+      if (matrix[i] != 0 && matrix[i] == matrix[i + 1] && matrix[i + 1] == matrix[i + 2]) {
+        // game ends
+        if (matrix[i] == 1) {
+          Serial.println("Player wins!");
+        } else {
+          Serial.println("Computer wins!");
+        }
+        gameDisplay();
+        return;
+      }
+    }
+    // inspect cols
+    for (int i = 0; i < 3; i ++) {
+      if (matrix[i] != 0 && matrix[i] == matrix[i + 3] && matrix[i + 3] == matrix[i + 6]) {
+        // game ends
+        if (matrix[i] == 1) {
+          Serial.println("Player wins!");
+        } else {
+          Serial.println("Computer wins!");
+        }
+        gameDisplay();
+        return;
+      }
+    }
+    // inspect diagnal
+    if (matrix[0] != 0 && matrix[0] == matrix[4] && matrix[4] == matrix[8]) {
+      // game ends
+      if (matrix[0] == 1) {
+        Serial.println("Player wins!");
+      } else {
+        Serial.println("Computer wins!");
+      }
+      gameDisplay();
+      return;
+    }
+    else if (matrix[2] != 0 && matrix[2] == matrix[4] && matrix[4] == matrix[6]) {
+      if (matrix[2] == 1) {
+        Serial.println("Player wins!");
+      } else {
+        Serial.println("Computer wins!");
+      }
+      gameDisplay();
+      return;
+    }
+    // if all filled, draw
+    for (int i = 0; i < 9; i ++) {
+      if (matrix[i] == 0) {
+        return;
+      }
+    }
+    Serial.println("Draw!");
+    gameDisplay();
+  } else {
+    // idle
+  }
+}
+
+void gameDisplay() {
+  for (int i = 2; i <= 13; i++) {
+    digitalWrite(i, LOW);
+  }
+  analogWrite(A0, 0);
+  analogWrite(A1, 0);
+  analogWrite(A2, 0);
+  analogWrite(A3, 0);
+  analogWrite(A4, 0);
+  analogWrite(A5, 0);
+  for (int i = 0; i < 9; i++) {
+    matrix[i] = 0;
+    indexToPinMapper(i, 0);
+    delay(200);
+    indexToPinMapper(i, 1);
+    delay(200);
+  }
+  for (int i = 2; i <= 13; i++) {
+    digitalWrite(i, LOW);
+  }
+  analogWrite(A0, 0);
+  analogWrite(A1, 0);
+  analogWrite(A2, 0);
+  analogWrite(A3, 0);
+  analogWrite(A4, 0);
+  analogWrite(A5, 0);
+  state = abs(state - 1);
+}
